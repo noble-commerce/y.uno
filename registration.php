64499449ef3d581f60c4d4037203fa401f173266
lucaszit <lucas.pereira080@gmail.com>
2025-06-10 22:57:31 -0300

    feature/NC-001 - Iniciando módulo Magento 2

---
+++ registration.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * Copyright © 2025 NobleCommerce. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+declare(strict_types=1);
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'NobleCommerce_Yuno',
+    __DIR__
+);
